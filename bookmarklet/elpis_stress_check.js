4e4e7051f6be671ee7b6aef4c555afb8c4f2f3f3
kaiba <kaibadash@gmail.com>
2024-07-23 09:31:52 +0900

    Add comment

--- bookmarklet/elpis_stress_check.js
+++ bookmarklet/elpis_stress_check.js
@@ -1,4 +1,5 @@
 javascript: (function () {
+  /* https://s-check.elpis.life/ のデフォルト値として回答が多そうな一番左のradioにチェックを入れる */
   var radios = document.querySelectorAll('input[type="radio"][value="1"]');
   radios.forEach(function (radio) {
     radio.click();
