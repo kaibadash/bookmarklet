a0f61e4e5bfcdc47487c282d5072cc103f30bfa3
kaiba <kaibadash@gmail.com>
2024-07-23 09:34:50 +0900

    ELPIS: convert span to label

--- bookmarklet/elpis_stress_check.js
+++ bookmarklet/elpis_stress_check.js
@@ -4,4 +4,16 @@ javascript: (function () {
   radios.forEach(function (radio) {
     radio.click();
   });
+
+  /* spanをlabelに変換する */
+  var radios = document.querySelectorAll('input[type="radio"]');
+  radios.forEach(function (radio) {
+    var span = radio.nextElementSibling;
+    if (span && span.tagName.toLowerCase() === "span") {
+      var label = document.createElement("label");
+      label.htmlFor = radio.id;
+      label.innerText = span.innerText;
+      span.parentNode.replaceChild(label, span);
+    }
+  });
 })();
