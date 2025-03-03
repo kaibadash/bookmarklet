a40fcceecb251bd4f727b671aee946774628bb28
kaiba <kaibadash@gmail.com>
2025-03-03 13:22:29 +0900

    Fix invalida month

--- tampermonkey/suica_yyyymmdd.user.js
+++ tampermonkey/suica_yyyymmdd.user.js
@@ -18,14 +18,7 @@
     const selectedYearMonth = selectYearMonth.value;
     // 年月の値を配列に分割
     let [year, month] = selectedYearMonth.split("/");
-    // 本日は月初だったら先月を選択
-    if (new Date().getDate() <= 5) {
-      month--;
-    }
-    if (month == 0) {
-      month = 12;
-      year--;
-    }
+
     // 月日セルを取得 (例: 10/01)
     const dateCells = document.querySelectorAll("td .whtbg");
     // 日付を更新
@@ -33,13 +26,23 @@
       const originalDate = cell.innerText; // 例: "10/01"
       if (/\d{2}\/\d{2}/.test(originalDate)) {
         const [m, d] = originalDate.split("/");
-        const fullDate = new Date(`${year}-${month}-${d}`);
+        const fullDate = new Date(`${year}-${m}-${d}`);
         const dayName = fullDate.toLocaleDateString("en-US", {
           weekday: "short",
         });
 
+        // 曜日に応じて色を設定（土日は赤色）
+        const dayOfWeek = fullDate.getDay();
+        if (dayOfWeek === 0 || dayOfWeek === 6) {
+          // 0=日曜日, 6=土曜日
+          cell.style.color = "red";
+        }
+
         // yyyy/mm/dd(Day)形式で表示
-        cell.innerText = `${year}/${month}/${d}(${dayName})`;
+        cell.innerText = `${year}/${m}/${d}(${dayName})`;
+
+        // 左寄せに設定
+        cell.style.textAlign = "left";
       }
     });
   }
