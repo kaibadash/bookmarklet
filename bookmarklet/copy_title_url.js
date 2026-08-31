6332b1b44a128316b502e39a3118f9b2066d7e6f
kaiba <kaibadash@gmail.com>
2026-08-31 12:47:28 +0900

    Support Fuxxing notion URL

--- bookmarklet/copy_title_url.js
+++ bookmarklet/copy_title_url.js
@@ -1,23 +1,39 @@
-javascript: var global = window;
-global.COPY_TO_CLIPBOARD = global.COPY_TO_CLIPBOARD || {};
-global.COPY_TO_CLIPBOARD.getUrlInfo = function () {
-  var a = new String(document.title);
-  a.allReplace = function (a) {
-    var b = this,
-      c;
-    for (c in a) b = b.replace(new RegExp(c, "g"), a[c]);
-    return b;
-  }.bind(a);
-  return a.allReplace({}) + "\n" + document.URL;
-};
-global.COPY_TO_CLIPBOARD.copyToClipboard = function () {
-  var a = document.createElement("textarea");
-  a.textContent = this.getUrlInfo();
-  var d = document.getElementsByTagName("body")[0];
-  d.appendChild(a);
-  a.select();
-  var b = document.execCommand("copy");
-  d.removeChild(a);
-  return b;
-};
-global.COPY_TO_CLIPBOARD.copyToClipboard();
+javascript: (function () {
+  var text = document.title + "\n" + location.href;
+  var listener = function (e) {
+    e.clipboardData.setData("text/plain", text);
+    e.preventDefault();
+  };
+  document.addEventListener("copy", listener);
+  var ok = false;
+  try {
+    ok = document.execCommand("copy");
+  } catch (e) {
+    console.error(e);
+  }
+  document.removeEventListener("copy", listener);
+
+  if (!ok) {
+    return;
+  }
+  var toast = document.createElement("div");
+  toast.textContent = "Copied!";
+  toast.style.cssText =
+    "position:fixed;top:20px;right:20px;background:rgba(0,100,0,0.8);" +
+    "color:#fff;padding:10px 16px;border-radius:6px;font-size:14px;" +
+    "z-index:999999;transition:opacity 0.3s;opacity:0;font-family:sans-serif;" +
+    "pointer-events:none;";
+
+  document.body.appendChild(toast);
+  setTimeout(function () {
+    toast.style.opacity = "1";
+  }, 10);
+  setTimeout(function () {
+    toast.style.opacity = "0";
+    setTimeout(function () {
+      if (toast.parentNode) {
+        toast.parentNode.removeChild(toast);
+      }
+    }, 300);
+  }, 2000);
+})();
